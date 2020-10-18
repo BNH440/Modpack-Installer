b577d8fb9a9cc576e72be3b1e83a0c6f221c58e8
Blake Haug <33404760+BNH440@users.noreply.github.com>
2020-10-17 20:13:07 -0700

    0.2.5

--- main.js
+++ main.js
@@ -63,5 +63,11 @@ const menu = Menu.buildFromTemplate(template)
 Menu.setApplicationMenu(menu)
 
 
-
-
+export default class AppUpdater {
+  constructor() {
+    const log = require("electron-log")
+    log.transports.file.level = "debug"
+    autoUpdater.logger = log
+    autoUpdater.checkForUpdatesAndNotify()
+  }
+}
